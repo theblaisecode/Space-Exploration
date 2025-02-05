cbf445634a7adb4d54f6934892affdaee8e77d7f
Blaise Nwachukwu <89015653+theblaisecode@users.noreply.github.com>
2025-02-05 06:13:51 +0100

    first commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,23 @@ import HomeWrapper from "../styles/HomeWrapper"
 
 function Home() {
   return (
-    <HomeWrapper>Home</HomeWrapper>
-  )
+    <HomeWrapper>
+      <div className="container">
+        <div className="homeContent">
+          <div className="contentTop">
+            <span>so, you want to travel to</span>
+            <h1>space</h1>
+            <p>
+              Let&apos;s face it; if you want to go to space, you might as well
+              genuinely go to outer space and not hover kind of on the edge of
+              it. Well sit back, and relax because we&apos;ll give you a truly out of
+              this world experience!
+            </p>
+          </div>
+        </div>
+      </div>
+    </HomeWrapper>
+  );
 }
+
 export default Home
