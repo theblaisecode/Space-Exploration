9f63367ec15e1bac5d1ed18807b591eea59a1dc6
Blaise Nwachukwu <89015653+theblaisecode@users.noreply.github.com>
2025-02-05 07:03:32 +0100

    first commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
-import HomeWrapper from "../styles/HomeWrapper"
+import { Link } from "react-router-dom";
+import HomeWrapper from "../styles/HomeWrapper";
 
 function Home() {
   return (
@@ -11,9 +12,17 @@ function Home() {
             <p>
               Let&apos;s face it; if you want to go to space, you might as well
               genuinely go to outer space and not hover kind of on the edge of
-              it. Well sit back, and relax because we&apos;ll give you a truly out of
-              this world experience!
+              it. Well sit back, and relax because we&apos;ll give you a truly
+              out of this world experience!
             </p>
+          </div>
+
+          <div className="contentBottom">
+            <div className="btnBg">
+              <Link to="/destination" className="homeBtn">
+                explore
+              </Link>
+            </div>
           </div>
         </div>
       </div>
@@ -21,4 +30,4 @@ function Home() {
   );
 }
 
-export default Home
+export default Home;
--- src/styles/HomeWrapper.tsx
+++ src/styles/HomeWrapper.tsx
@@ -10,6 +10,80 @@ const HomeWrapper = styled.section`
   justify-content: center;
   color: var(--textLight);
   position: relative;
+
+  .homeContent {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: 25rem;
+    /* gap: 3rem; */
+  }
+
+  .contentTop {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    gap: 2.4rem;
+  }
+
+  .contentTop h1,
+  .contentBottom .homeBtn,
+  .contentTop span {
+    text-transform: uppercase;
+    font-family: "Bellefair", serif;
+    font-size: 14.4rem;
+  }
+
+  .contentTop h1 {
+    font-weight: 500;
+  }
+
+  .contentTop span {
+    font-family: "Barlow Condensed", serif;
+    font-size: 2.8rem;
+    line-height: auto;
+    font-weight: 400;
+    letter-spacing: 0.4rem;
+    color: var(--textPrimary);
+  }
+
+  .contentTop p {
+    font-family: "Barlow", serif;
+    font-size: 1.8rem;
+    font-weight: 200;
+    line-height: 180%;
+    width: 80%;
+    color: var(--textPrimary);
+  }
+
+  .contentBottom {
+    flex: 1;
+    display: flex;
+    justify-content: flex-end;
+  }
+
+  .btnBg {
+    background: transparent;
+    padding: 8rem;
+    border-radius: 50%;
+    transition: 0.9s;
+  }
+
+  .btnBg:hover {
+    background: rgba(225, 225, 225, 0.15);
+    transition: all 0.9s;
+  }
+
+  .contentBottom .homeBtn {
+    display: block;
+    font-size: 3.2rem;
+    color: var(--textDark);
+    background: var(--textLight);
+    border: none;
+    outline: none;
+    padding: 11.75rem 6.8rem;
+    border-radius: 50%;
+  }
 `;
 
 export default HomeWrapper;
