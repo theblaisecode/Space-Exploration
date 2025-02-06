e3e7f618995a37632ef0fdf171a3180c29f09718
Blaise Nwachukwu <89015653+theblaisecode@users.noreply.github.com>
2025-02-06 17:51:37 +0100

    first commit

--- src/layout/HomeLayout.tsx
+++ src/layout/HomeLayout.tsx
@@ -1,12 +1,25 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "../components/Header";
+import { useEffect, useState } from "react";
 
 function HomeLayout() {
+  const location = useLocation(); // Detects route changes
+  const [fade, setFade] = useState(false);
+
+  useEffect(() => {
+    setFade(true);
+    const timeout = setTimeout(() => setFade(false), 900); // Match animation duration
+    return () => clearTimeout(timeout);
+  }, [location]);
+
   return (
     <>
       <Header />
-      <Outlet />
+      <div className={fade ? "fadeIn" : ""}>
+        <Outlet />
+      </div>
     </>
   );
 }
+
 export default HomeLayout;
