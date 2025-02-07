36940e663367e18e956bda5695e735ef3a3448b4
Blaise Nwachukwu <89015653+theblaisecode@users.noreply.github.com>
2025-02-07 10:54:01 +0100

    first commit

--- src/data.ts
+++ src/data.ts
@@ -44,6 +44,7 @@ export const destination: {
   id: number;
   tabName: string;
   img: string;
+  imgAlt:string;
   heading: string;
   tabContent: string;
   distance: string;
@@ -53,6 +54,7 @@ export const destination: {
     id: 1,
     tabName: "moon",
     img: moon,
+    imgAlt: "moon",
     heading: "moon",
     tabContent:
       "See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.",
@@ -63,6 +65,7 @@ export const destination: {
     id: 2,
     tabName: "mars",
     img: mars,
+    imgAlt: "mars",
     heading: "mars",
     tabContent:
       "Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!",
@@ -73,6 +76,7 @@ export const destination: {
     id: 3,
     tabName: "europa",
     img: europa,
+    imgAlt: "europa",
     heading: "europa",
     tabContent:
       "The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.",
@@ -83,6 +87,7 @@ export const destination: {
     id: 4,
     tabName: "titan",
     img: titan,
+    imgAlt: "itian",
     heading: "titan",
     tabContent:
       "The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.",
--- src/pages/Destination.tsx
+++ src/pages/Destination.tsx
@@ -1,6 +1,46 @@
+import { destination } from "../data";
 import DestinationWrapper from "../styles/DestinationWrapper";
 
 function Destination() {
-  return <DestinationWrapper>Destination</DestinationWrapper>;
+  return (
+    <DestinationWrapper>
+      <div className="container">
+        <div className="destinationContent">
+          {destination.map((item) => {
+            const {
+              id,
+              tabName,
+              img,
+              imgAlt,
+              heading,
+              tabContent,
+              distance,
+              travelTime,
+            } = item;
+
+            return (
+              <>
+                <div key={id} className="contentTop">
+                  <img src={img} alt={imgAlt} />
+                </div>
+
+                <div className="contentBottom">
+                  <div className="destinationTab">
+                    {destination.map((tabNames, index) => {
+                      return (
+                        <span key={index} className="tabs activetab">
+                          {tabNames.tabName}
+                        </span>
+                      );
+                    })}
+                  </div>
+                </div>
+              </>
+            );
+          })}
+        </div>
+      </div>
+    </DestinationWrapper>
+  );
 }
 export default Destination;
