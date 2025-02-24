7e7d2d10b761844eae5c1fef45b6874be3b0283b
Blaise Nwachukwu <89015653+theblaisecode@users.noreply.github.com>
2025-02-24 17:36:36 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,33 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Home, Destination, Crew, Technology } from "./pages/index";
 import HomeLayout from "./layout/HomeLayout";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <HomeLayout />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-      {
-        path: "/destination",
-        element: <Destination />,
-      },
-      {
-        path: "/crew",
-        element: <Crew />,
-      },
-      {
-        path: "/technology",
-        element: <Technology />,
-      },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <HomeLayout />,
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+        {
+          path: "/destination",
+          element: <Destination />,
+        },
+        {
+          path: "/crew",
+          element: <Crew />,
+        },
+        {
+          path: "/technology",
+          element: <Technology />,
+        },
+      ],
+    },
+  ],
+  { basename: "/Space-Exploration" }
+);
 
 function App() {
   return <RouterProvider router={router} />;
